9fcfcf52f509622049db0a7ad889aba8b6117f2c
Mike Gaudreau <gaudreaum01@gmail.com>
2020-01-14 10:19:03 -0500

    Add the new routes for the controller just created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+
+      resources :user
+      resources :polls do
+        resources :questions do
+          resources :answers
+          resources :submissions
+        end
+      end
+    end
+  end
 end
