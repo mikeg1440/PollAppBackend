69bbad36e413a563cfa50271a716bcb3ab942c42
Mike Gaudreau <gaudreaum01@gmail.com>
2020-01-15 14:33:40 -0500

    Add content to the seed file to seed for testing purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+# Create a poll for favorite movies
+
+potter_poll = Poll.create(title: "Favorite Harry Potter", author: Faker::Name.first_name, question: 'What is your all time favorite Harry Potter movie?')
+
+(1..3).each {|num| potter_poll.answers.build(content: Faker::Movies::HarryPotter.book)}
+
+potter_poll.save
+
+
+# Create a poll for favorite Star Wars quote
+
+starwars_poll = Poll.create(title: 'Best Star Wars Quote', author: Faker::Name.first_name, question: 'What is the best quote in the Star Wars saga?')
+
+(1..5).each {|num| starwars_poll.answers.build(content: Faker::Movies::StarWars.quote)}
+
+starwars_poll.save
+
+
+# Create a poll for favorite charecter in the Lord of the Rings stories
+
+lor_poll = Poll.create(title: 'Favorite Lord of The Rings Charecter', author: Faker::Name.first_name, question: 'Who is your favorite charecter in the Lord of the Rings stories?')
+
+(1..6).each {|num| lor_poll.answers.build(content: Faker::Movies::LordOfTheRings.character)}
+
+lor_poll.save
